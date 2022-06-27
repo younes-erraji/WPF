f02fba50fc2e1b2488a403a818ee961b63234d1d
Younes-Erraji <younes.erraji8@gmail.com>
2022-06-27 14:38:14 +0100

    MarkupExtensions & Binding & Controls

---
+++ WPF_Demo/Views/Controls/ControlsWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_Demo.Views.Controls
+{
+    /// <summary>
+    /// Interaction logic for ControlsWindow.xaml
+    /// </summary>
+    public partial class ControlsWindow : Window
+    {
+        public ControlsWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // LabelNumber.Content = ComboBoxNumbers.SelectedItem.ToString();
+            LabelNumber.Content = ComboBoxNumbers.SelectionBoxItem.ToString();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Uri uri = new Uri("../../Assets/Images/Photoshop.PNG", UriKind.Relative);
+            BitmapImage bitmap = new BitmapImage(uri);
+            Logo.Source = bitmap;
+        }
+    }
+}
---
+++ WPF_Demo/Views/MarkupExtensions/ShowTime.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Markup;
+
+namespace WPF_Demo.Views.MarkupExtensions
+{
+    class ShowTime: MarkupExtension
+    {
+        public string Header { get; set; }
+
+        public ShowTime() { }
+
+        public ShowTime(string Header)
+        {
+            this.Header = Header;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return string.Format($"{Header}: {DateTime.Now.ToLongTimeString()}");
+        }
+    }
+}
