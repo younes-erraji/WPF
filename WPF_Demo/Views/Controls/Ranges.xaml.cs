a1dba4c2ffdb5255ce39e3e92f3ce1dc6fa7157e
Younes-Erraji <younes.erraji8@gmail.com>
2022-07-01 11:14:46 +0100

    Ranges

---
+++ WPF_Demo/Views/Controls/Ranges.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_Demo.Views.Controls
+{
+    /// <summary>
+    /// Interaction logic for Ranges.xaml
+    /// </summary>
+    public partial class Ranges : Window
+    {
+        public Ranges()
+        {
+            InitializeComponent();
+        }
+
+        private void NextStepButton_Click(object sender, RoutedEventArgs e)
+        {
+            progress.Value += 5;
+            VProgress.Value += 5;
+        }
+    }
+}
